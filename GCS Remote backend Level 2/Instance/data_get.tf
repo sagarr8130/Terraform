a51bb0e7f9f2091a3ceb88c113c6aaccdb962eda
sagarr8130 <61772142+sagarr8130@users.noreply.github.com>
2020-04-28 19:27:25 +0530

    Delete data_get.tf

--- GCS Remote backend Level 2/Instance/data_get.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "myvm" {
-  backend = "gcs"
-  config = {
-    bucket = "new-bucket-terraform"
-    credentials = "account.json"
-    prefix = "terraform.tfstate"
-  }
-}
